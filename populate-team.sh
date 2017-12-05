6002eb1dbe79814ce2144f5aa8a81fd75c716d8b
probinso <pmoss.robinson@gmail.com>
2017-12-04 18:39:00 -0800

    team populator

---
+++ populate-team.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if (( $# != 3)); then
+    printf "Usage: %s <credentials.json> <teamname> <userlist.txt>\n" "$0" >&2;
+    exit 1;
+fi;
+
+cred_file=$1;
+team=$2;
+ifname=$3;
+
+USER=$(cat ${cred_file} | jq --raw-output '.username')
+PASS=$(cat ${cred_file} | jq --raw-output '.password')
+
+
+for uname in $(cat ${ifname}); do
+    curl -i \
+         -H "application/vnd.github.v3+json" \
+         --user "${USER}:${PASS}" \
+         --request PUT \
+         --data '{"permission":"push"}' \
+         "https://api.github.com/repos/PortlandDataScienceGroup/${team}/collaborators/${uname}"
+done;
