46437a60d3220e320ee40fd63d89a287552754dd
probinso <pmoss.robinson@gmail.com>
2017-12-04 19:31:04 -0800

    safe execute

--- fetch-all.sh
+++ fetch-all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 for color in $(cat teams.txt);
 do
     git fetch ${color} master
--- push-all.sh
+++ push-all.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+git checkout master
+
 for color in $(cat ./teams.txt);
 do
     git push -u ${color} master
