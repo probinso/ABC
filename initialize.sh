84741992bbe754be37c34369c18fcbc021151231
probinso <pmoss.robinson@gmail.com>
2017-12-04 16:37:33 -0800

    update initialize to include origin

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
+
 for color in $(cat ./teams.txt);
 do
     git remote add ${color} git@github.com:PortlandDataScienceGroup/${color}.git
 done;
+
+git remote add origin git@github.com:PortlandDataScienceGroup/ABC.git
