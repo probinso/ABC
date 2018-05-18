0c01e2c3f3ac94d0402b93aac741a693112550e9
Philip <pmoss.robinson@gmail.com>
2018-05-18 08:42:20 -0700

    Update populate-team.sh

--- populate-team.sh
+++ populate-team.sh
@@ -2,24 +2,25 @@
 
 set -euo pipefail
 
-if (( $# != 3)); then
-    printf "Usage: %s <credentials.json> <teamname> <userlist.txt>\n" "$0" >&2;
+if (( $# != 2)); then
+    printf "Usage: %s <credentials.json> <teams.txt>\n" "$0" >&2;
     exit 1;
 fi;
 
 cred_file=$1;
-team=$2;
-ifname=$3;
+TEAMS=$2;
 
 USER=$(cat ${cred_file} | jq --raw-output '.username')
 PASS=$(cat ${cred_file} | jq --raw-output '.password')
 ORG=$(cat ${cred_file} | jq --raw-output '.organization')
 
-for uname in $(cat ${ifname}); do
-    curl -i \
-         -H "application/vnd.github.v3+json" \
-         --user "${USER}:${PASS}" \
-         --request PUT \
-         --data '{"permission":"push"}' \
-         "https://api.github.com/repos/${ORG}/${team}/collaborators/${uname}"
+for team in $(cat $TEAMS); do 
+    for uname in $(cat ${team}.txt); do
+        curl -i \
+             -H "application/vnd.github.v3+json" \
+             --user "${USER}:${PASS}" \
+             --request PUT \
+             --data '{"permission":"push"}' \
+             "https://api.github.com/repos/${ORG}/${team}/collaborators/${uname}"
+    done;
 done;
